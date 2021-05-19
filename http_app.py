25c5a5bd365b4f0c43acdea079aaaed30260da49
VladislavBakaev <57841320+VladislavBakaev@users.noreply.github.com>
2021-05-19 12:18:24 +0300

    change http port

--- http_app.py
+++ http_app.py
@@ -3,4 +3,4 @@ from http_control import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='localhost', port=8000)
+    app.run(debug=False, host='localhost', port=8002)
